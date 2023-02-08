6cfdd769c6500d6b16d5c9d64202224ac959ccd7
Michal <noszczykmichal@wp.pl>
2023-02-08 21:44:49 +0100

    added new icons

--- src/components/Layout/Icon/Icon.js
+++ src/components/Layout/Icon/Icon.js
@@ -1,15 +1,42 @@
 import PropTypes from "prop-types";
 
+import CommunicationSkills from "./Icons/CommunicationSkills";
+import CSS from "./Icons/CSS";
+import Git from "./Icons/Git";
 import Github from "./Icons/Github";
+import Html from "./Icons/Html";
+import LikeToLearn from "./Icons/LikeToLearn";
 import Linkedin from "./Icons/Linkedin";
+import React from "./Icons/React";
+import Sass from "./Icons/Sass";
+import Teamwork from "./Icons/Teamwork";
+import ToDoList from "./Icons/ToDoList";
 import DefaultIcon from "./Icons/DefaultIcon";
 
 function Icon({ name }) {
   switch (name) {
+    case "CommunicationSkills":
+      return <CommunicationSkills />;
+    case "CSS":
+      return <CSS />;
+    case "Git":
+      return <Git />;
     case "Github":
       return <Github />;
+    case "Html":
+      return <Html />;
+    case "LikeToLearn":
+      return <LikeToLearn />;
     case "Linkedin":
       return <Linkedin />;
+    case "React":
+      return <React />;
+    case "Sass":
+      return <Sass />;
+    case "Teamwork":
+      return <Teamwork />;
+    case "ToDoList":
+      return <ToDoList />;
     default:
       return <DefaultIcon />;
   }
---
+++ src/components/Layout/Icon/Icons/CSS.js
@@ -0,0 +1,18 @@
+function CSS() {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="-10 -5 1034 1034"
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+    >
+      <path
+        fill="#000000"
+        d="M190 226l-30 149h604l-19 96h-604l-30 148h604l-33 169l-244 81l-211 -81l15 -73h-149l-35 178l349 133l402 -133l133 -667h-752z"
+      />
+    </svg>
+  );
+}
+
+export default CSS;
