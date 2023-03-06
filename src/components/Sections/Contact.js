c15522ce1329d1c612dbab1e64c87f3c16de9e8c
Michal <noszczykmichal@wp.pl>
2023-03-07 00:38:12 +0100

    added scrollreveal to the Contact section

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -1,9 +1,27 @@
-import { email } from "../../utils/config";
+import { useRef, useEffect } from "react";
+import ScrollReveal from "scrollreveal";
+
+import { email, srConfig } from "../../utils/config";
+import usePrefersReducedMotion from "../../utils/usePrefersReducedMotion";
 import "./Contact.scss";
 
 function Contact() {
+  const revealContainer = useRef();
+  const prefersReducedMotion = usePrefersReducedMotion();
+  const prefersReducedMotionRef = useRef(prefersReducedMotion);
+
+  useEffect(() => {
+    if (prefersReducedMotionRef.current) {
+      return undefined;
+    }
+
+    ScrollReveal().reveal(revealContainer.current, srConfig());
+
+    return () => {};
+  }, []);
+
   return (
-    <section id="contact" className="contact">
+    <section id="contact" className="contact" ref={revealContainer}>
       <h2 className="numbered-heading overline">What&apos;s Next?</h2>
       <h2 className="title">Get In Touch</h2>
       <p>
