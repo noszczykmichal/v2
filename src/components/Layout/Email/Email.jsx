901a5ea8f26defe0c6683e51ca8041c24c722a7f
Michal Noszczyk <michal.noszczyk@publicisgroupe.com>
2024-05-23 12:59:50 +0200

    added firebase analytics

--- src/components/Layout/Email/Email.jsx
+++ src/components/Layout/Email/Email.jsx
@@ -1,12 +1,17 @@
 import Aside from "../Aside/Aside";
-import { email } from "../../../utils/config";
+import { email, linkAnalyticsHandler } from "../../../utils/config";
 import "./Email.scss";
 
 function Email() {
   return (
     <Aside orientation="right">
       <div className="email-container">
-        <a href={`mailto:${email}`}>{email}</a>
+        <a
+          href={`mailto:${email}`}
+          onClick={linkAnalyticsHandler("side_navigation", "email")}
+        >
+          {email}
+        </a>
       </div>
     </Aside>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import App from "./App";
-import "./index.scss";
 import { UIContextProvider } from "./store/uiContext";
+import "./index.scss";
 
 const client = new ApolloClient({
   uri: "https://api.crystallize.com/noszczykmichal/catalogue/",
   cache: new InMemoryCache(),
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdS8Se_1gNn2590iYP8KnFGtosXPqWVKI",
+  authDomain: "portfolio-8ff44.firebaseapp.com",
+  projectId: "portfolio-8ff44",
+  storageBucket: "portfolio-8ff44.appspot.com",
+  messagingSenderId: "1032156294818",
+  appId: "1:1032156294818:web:bcda5a84940f495fa0f1fb",
+  measurementId: "G-W4CJCB0ETY",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -24,7 +39,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
